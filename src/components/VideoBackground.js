50de240211ea51765b62f4d96002764163790737
Yash Paota <yashpaota@gmail.com>
2023-11-08 15:55:17 +0530

    Added video title and background video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { API_OPTIONS } from '../utils/constants';
-import { useEffect } from 'react';
-
-const VideoBackground = ({movieId}) => {
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+const VideoBackground = ({ movieId }) => {
   //fetch movie trailer
   //movieid
-
-  const getMovieVideo = async() => {
-      const data = await fetch("https://api.themoviedb.org/3/movie/299054/videos?language=en-US", API_OPTIONS);
-      const json = await data.json();
-      // console.log(json);
-      const filterData = json.results.filter((video) => video.type === "Trailer"); //Here We got two trailers
-      console.log(filterData);
-      const trailer = filterData[0];   //We are assuming
-  }
-
-  useEffect(() => {
-     getMovieVideo();
-  },[])
+  useMovieTrailer(movieId); 
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+  
   return (
-    <div>VideoBackground</div>
-  )
-}
+    <div className="w-screen">
+      <iframe
+        className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key +"?&autoplay=1&mute=1"}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
 
 export default VideoBackground;
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const VideoTitle = ({original_title , overview}) => {
   return (
-    <div className="pt-36 px-12">
+    <div className=" w-screen aspect-video pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black ">
         <h1 className="text-4xl font-bold">{original_title}</h1>
         <p className='py-6 text-lg w-1/4'>{overview}</p>
         <div>
-            <button className='bg-gray-500 p-2 text-white px-12 text-xl opacity-50 rounded-md'>Play</button>
-            <button className='mx-2 bg-gray-500 p-2 text-white px-12 text-xl opacity-50 rounded-md'>More Info</button>
+            <button className='bg-white p-2 text-black px-12 text-xl rounded-md font-bold hover:bg-opacity-80'>Play</button>
+            <button className='mx-2  bg-gray-500 p-2 text-white px-12 text-xl opacity-75 rounded-md'>More Info</button>
         </div>
     </div>
   )
