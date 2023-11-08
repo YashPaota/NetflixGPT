7e542e7048bd01f16ab2425540fb4993b41edeaf
Yash Paota <yashpaota@gmail.com>
2023-11-08 12:53:08 +0530

    extracted fetch concept from browse and added in custom hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider, createBrowserRouter} from "react-router-dom";
-import {onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useDispatch } from "react-redux";
-import {addUser, removeUser} from "../utils/userSlice";
+
 
 
 const Body = () => {
-  const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -22,23 +17,7 @@ const Body = () => {
     },
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        //user signs in
-        const {uid , email , displayName , photoURL} = user;
-        dispatch(addUser({uid:uid , email:email , displayName : displayName , photoURL: photoURL}));
-        
-
-        
-      } else {
-        // User is signed out
-        dispatch(removeUser());
-        // navigate("/");
-        
-      }
-    });
-  }, []);
+  
   return (
     <div>
       <RouterProvider router={appRouter} />
