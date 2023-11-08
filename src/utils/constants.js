c0a290573e038f0ccc3bf3c45a0f929950627e8c
Yash Paota <yashpaota@gmail.com>
2023-11-08 17:44:29 +0530

    added cdn of img

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,3 +14,6 @@ export const API_OPTIONS = {
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
+
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
