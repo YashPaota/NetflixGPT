53aa1c1848b740b476a9aa06f72edd095b711ea8
Yash Paota <yashpaota@gmail.com>
2023-11-08 10:09:40 +0530

    Extracted the onAuth logic from body to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,67 @@
-import React from "react";
+// import React from "react";
 import { LOGO_URL } from "../utils/constants";
-import {signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import React from "react";
+import { useEffect } from "react";
 
 const Header = () => {
-
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
   const handleSignOut = () => {
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      navigate("/");
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        // navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
 
-    }).catch((error) => {
-      navigate("/error");
+  useEffect(() => {
+   const unsubscribe =  onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //user signs in
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
     });
-  }
+    return () => unsubscribe();
+  }, []);
+  
   return (
     <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between ">
-      <img
-        className="w-44"
-        src={LOGO_URL}
-        alt="Netflix Logo"
-      />
-      <div className="flex p-2">
-        <img  className="w-10 h-10  " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ3SFVmXoYNHl2D22fjIEAwMuEqrbDYiUUwsWi6-K0AEnh9QZzAhgaOayZ6hFG4Eh_1m4&usqp=CAU" alt="usericon" />
-         <button onClick = {handleSignOut} className="ml-2 mb-3 text-white font-bold text-center">Sign Out</button>
-      </div>
+      <img className="w-44" src={LOGO_URL} alt="Netflix Logo" />
+
+      {user && (
+        <div className="flex p-2">
+          <img className="w-10 h-10  " src={user?.photoURl} alt="usericon" />
+          <button
+            onClick={handleSignOut}
+            className="ml-2 mb-3 text-white font-bold text-center"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
