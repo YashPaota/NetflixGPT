419bb4e19248e0df55ffe7852d54dfcc59fd0c18
Yash Paota <yashpaota@gmail.com>
2023-11-08 12:51:03 +0530

    Added Api Options in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,16 @@
 export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const BACKGROUND_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDdmYjgxODA4Y2YzMTAyYWM4ODllODU4NTQ4ZTdjNyIsInN1YiI6IjY1NGIxMjgwMjg2NmZhMDExYmQxMzc0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzhEXQwPGC9PU-qe5AUM7BW9dLlclFIQg3TcT6730Zc'
+    }
+  };
+  
+//   fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
