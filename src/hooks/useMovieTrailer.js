3db1b7e9223c5f922bdee9a95bc63300acf542ce
Yash Paota <yashpaota@gmail.com>
2023-11-08 15:53:04 +0530

    Added a custoom hook

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+import { API_OPTIONS } from "../utils/constants";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filterData = json.results.filter((video) => video.type === "Trailer"); //Here We got two trailers
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+};
+
+export default useMovieTrailer;
