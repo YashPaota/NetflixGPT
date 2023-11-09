210993f51599c60aa5530b7db27c104aba5d0d33
Yash Paota <yashpaota@gmail.com>
2023-11-09 16:33:33 +0530

    Completed the GPT searchbar logic

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import {lang} from "../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 const GptSearchBar = () => {
+    const langKey = useSelector((store) => store.config.lang);
   return (
     <div className="pt-[10%] flex justify-center">
       <form className="w-1/2 bg-black grid grid-cols-12 ">
         <input
           className="m-4 py-2 px-4 col-span-9 rounded-sm"
           type="text"
-          placeholder={lang.hindi.gptSearchPlaceHolder}
+          placeholder={lang[langKey].gptSearchPlaceHolder}
         />
         <button className="bg-red-700 rounded-sm text-white m-4 py-2 px-4 col-span-3">
-          {lang.hindi.search}
+          {lang[langKey].search}
         </button>
       </form>
     </div>
