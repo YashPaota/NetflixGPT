be4919a31538608b229aeea3df05151057e5cf99
Yash Paota <yashpaota@gmail.com>
2023-11-09 16:32:24 +0530

    Added language contants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,3 +17,5 @@ export const API_OPTIONS = {
 
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const OPENAI_KEY = "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";
--- src/utils/languageConstants.js
+++ src/utils/languageConstants.js
@@ -2,13 +2,15 @@ export const lang = {
   en: {
     search: "Search",
     gptSearchPlaceHolder: "What would you like to watch today",
-  },
+ },
   hindi: {
     search: "खोज",
     gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे",
   },
-  spanish: "buscar",
+  spanish: { 
+  search : "buscar",
   gptSearchPlaceHolder: "¿Qué te gustaría ver hoy?",
+}
 };
 
 export const SUPPORTING_LANGUAGES = [
