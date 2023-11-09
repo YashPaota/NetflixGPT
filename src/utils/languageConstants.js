efbdb83ebd0de62212f9b5a051597658101caa27
Yash Paota <yashpaota@gmail.com>
2023-11-09 12:48:39 +0530

    Added langauage constans for languages feature

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,18 @@
+export const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceHolder: "What would you like to watch today",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे",
+  },
+  spanish: "buscar",
+  gptSearchPlaceHolder: "¿Qué te gustaría ver hoy?",
+};
+
+export const SUPPORTING_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
