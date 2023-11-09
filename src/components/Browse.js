26aeaa7f4cd283f8d11945c086c518147a329bbd
Yash Paota <yashpaota@gmail.com>
2023-11-09 10:57:52 +0530

    Called custom hooks and added more movie lists

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,8 +3,18 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingmovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import useTopRatedSeries from "../hooks/useTopRatedSeries";
+import useAiringTodaySeries from "../hooks/useAiringTodaySeries";
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
+  useTopRatedSeries();
+  useAiringTodaySeries();
 
   return (
     <div>
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -4,14 +4,19 @@ import { useSelector } from 'react-redux';
 
 const SecondaryContainer = () => {
   const movies= useSelector((store) => store.movies);
+  // console.log(movies.topRatedSeries);
   return (
     movies.nowPlayingMovies && (
-    <div>
-       <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-       <MovieList title="Trending" movies={movies.nowPlayingMovies} />
-       <MovieList title="Popular" movies={movies.nowPlayingMovies} />
-       <MovieList title="Upcoming Movies" movies={movies.nowPlayingMovies} />
-       <MovieList title="New Released" movies={movies.nowPlayingMovies} />
+    <div className='bg-black'>
+      <div className='-mt-32 relative z-20 pl-12'>
+        <MovieList title="Top Rated Movies" movies={movies.topRatedMovies} />
+        <MovieList title="Top Rated Series" movies={movies.topRatedSeries} />
+        <MovieList title="Popular" movies={movies.popularMovies} />
+        <MovieList title="Now Playing Movies" movies={movies.nowPlayingMovies} />
+        <MovieList title="Airing Today Series" movies={movies.airingTodaySeries} />
+        <MovieList title="Upcoming Movies" movies={movies.upcomingMovies} />
+        <MovieList title="New Released" movies={movies.nowPlayingMovies} />
+      </div>
     </div>
   )
   )
