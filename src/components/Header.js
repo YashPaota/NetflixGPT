dc7bc293ff8bd8bb61c6a7adcf24ac3c2e6e9b53
Yash Paota <yashpaota@gmail.com>
2023-11-09 16:32:58 +0530

    Added butoon and select options

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,7 @@ const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -65,8 +66,8 @@ const Header = () => {
 
       {user && (
         <div className="flex p-2">
-
-         <select className="py-2 px-4 m-2 bg-green-800 rounded-md text-white" onChange={handleLanguageChange}>
+        {showGptSearch && 
+          (  <select className="py-2 px-4 m-2 bg-green-800 rounded-md text-white" onChange={handleLanguageChange}>
             {SUPPORTING_LANGUAGES.map((lang) => 
               <option
                 className="bg-green-950" 
@@ -75,19 +76,20 @@ const Header = () => {
                 >
                 {lang.name}
                 </option> )}
-         </select>
+         </select> )
+         }
 
           <button 
-            className="px-2 m-2 py-2 bg-purple-800 text-white rounded-md mr-6"
+            className="px-2 m-2 py-2 bg-purple-800 text-white rounded-md mr-2"
             onClick={handleGptSearchClick} 
              >
-             GPT Search
+             {showGptSearch ? "Home Page" : "Launch GPT"}
              </button>
 
-          <img className="w-10 h-10  " src={user?.photoURl} alt="usericon" />
+          {/* <img className="w-10 h-10  " src={user?.photoURl} alt="usericon" /> */}
           <button
             onClick={handleSignOut}
-            className="px-2 m-2 py-2 text-white bg-red-700 rounded-md"
+            className="px-4 m-2 py-2 text-white bg-red-700 rounded-md"
           >
             Sign Out
           </button>
